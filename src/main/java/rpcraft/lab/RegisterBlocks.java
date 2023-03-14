cebb65289b3f6fe3d867f82d5a3f7ba14501605a
Lee Allen <leezallen@gmail.com>
2023-03-14 06:16:34 +0000

    Still getting started...
    Added a new battery item with sound on redstone and use action
    
    Added creative mode tabs
    
    Added hardness for block definitions
    
    Added loottables for dropping items
    
    Added language files

--- src/main/java/rpcraft/lab/RegisterBlocks.java
+++ src/main/java/rpcraft/lab/RegisterBlocks.java
@@ -1,30 +1,52 @@
 package rpcraft.lab;
 
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import rpcraft.lab.blocks.BatteryBlock;
 
 import static rpcraft.lab.LabMain.MODID;
 
 
 public class RegisterBlocks {
 	
-	public static final Block HAZARD = new Block(FabricBlockSettings.of(Material.METAL));
+	// Define base block details
+	public static final Block HAZARD = new Block(FabricBlockSettings.of(Material.METAL).strength(1f, 1f));
+	public static final BatteryBlock BATTERY = new BatteryBlock(FabricBlockSettings.of(Material.METAL).strength(3f, 3f));
+
+	// Define custom creative tabs. 
+	public static final ItemGroup DECORATIVE = FabricItemGroupBuilder.create(
+	        new Identifier(MODID, "rpcdecorative"))
+	        .icon(() -> new ItemStack(HAZARD))
+	        .build();
 	
-	public static void AddLabBlock (Block block, String name)
+	public static final ItemGroup ELECTRICAL = FabricItemGroupBuilder.create(
+	        new Identifier(MODID, "rpcelectrical"))
+	        .icon(() -> new ItemStack(BATTERY))
+	        .build();
+
+	
+	public static void AddLabBlock (Block block, String name, ItemGroup tab)
 	{
 		Identifier blockID = new Identifier(MODID, name);
 		Registry.register(Registry.BLOCK,blockID,block);
-		Registry.register(Registry.ITEM,blockID, new BlockItem(block, new FabricItemSettings()));
+		Registry.register(Registry.ITEM,blockID, new BlockItem(block, new FabricItemSettings().group(tab)));
 	}
 	
 	public static void RegisterAllBlocks()
 	{
-		AddLabBlock(HAZARD,"hazard");
+		
+		AddLabBlock(HAZARD,"hazard",DECORATIVE);
+		AddLabBlock(BATTERY,"battery",ELECTRICAL);
+		//AddLabBlock(HAZARD,"hazard",ItemGroup.DECORATIONS);
+		//AddLabBlock(BATTERY,"battery",ItemGroup.DECORATIONS);
 	}
 
 }
---
+++ src/main/java/rpcraft/lab/blocks/BatteryBlock.java
@@ -0,0 +1,61 @@
+package rpcraft.lab.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class BatteryBlock extends Block {
+	
+	public static final BooleanProperty ON = BooleanProperty.of("on");
+
+	public BatteryBlock (Settings settings) {
+		super (settings);
+		setDefaultState(this.stateManager.getDefaultState().with(ON, false));
+	}
+	
+	@Override
+	public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+	
+		if (!world.isClient) {
+			player.sendMessage(Text.of("Bzzzzzzzz..."), false);
+		}
+		
+		return ActionResult.SUCCESS;
+	}
+	
+    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify)
+    {
+        if (!world.isClient)
+        {
+            if (world.isReceivingRedstonePower(pos))
+            {
+            	if (!world.getBlockState(pos).get(ON) && !world.isClient) { 
+            		world.playSound(null, pos, SoundEvents.BLOCK_ANVIL_LAND, SoundCategory.BLOCKS, 1f, 1f);
+            	}
+            	world.setBlockState(pos, state.with(ON, true));
+            }
+            else
+            {
+                world.setBlockState(pos, state.with(ON, false));
+            }
+        }
+    }
+    
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(Properties.HORIZONTAL_FACING);
+        builder.add(ON);
+    }
+	
+}
